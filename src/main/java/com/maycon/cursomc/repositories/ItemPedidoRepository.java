14a5a1cd711405ffb0451b9a1fc75aae3a708cb7
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-24 21:32:00 +0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/maycon/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.maycon.cursomc.repositories;
+
+import com.maycon.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
