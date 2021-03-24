b66be68203264b849fcc928e5d396573296310ea
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-24 10:54:19 -0300

    endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/maycon/cursomc/resources/ClienteResource.java
@@ -0,0 +1,25 @@
+package com.maycon.cursomc.resources;
+
+import com.maycon.cursomc.domain.Cliente;
+import com.maycon.cursomc.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+    @Autowired
+    private ClienteService ClienteService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Cliente obj = ClienteService.buscar(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
---
+++ src/main/java/com/maycon/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.maycon.cursomc.services;
+
+import com.maycon.cursomc.domain.Cliente;
+import com.maycon.cursomc.repositories.ClienteRepository;
+import com.maycon.cursomc.services.Exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository ClienteRepository;
+
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> optional = ClienteRepository.findById(id);
+        return optional.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()
+        ));
+    }
+}
