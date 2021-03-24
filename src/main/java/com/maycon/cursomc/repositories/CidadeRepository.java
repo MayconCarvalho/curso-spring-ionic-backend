72049a15e8cfae44edaf9b665e1980693917d9ce
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-24 09:21:51 -0300

    criado componentes Estado e Cidade

---
+++ src/main/java/com/maycon/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.maycon.cursomc.repositories;
+
+import com.maycon.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
---
+++ src/main/java/com/maycon/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.maycon.cursomc.repositories;
+
+import com.maycon.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
