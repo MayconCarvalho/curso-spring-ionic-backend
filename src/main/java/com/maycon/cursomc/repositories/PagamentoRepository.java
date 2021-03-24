82c6c55ddf1f8533ffcf8db5049aba298e47136a
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-24 11:38:28 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/maycon/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.maycon.cursomc.repositories;
+
+import com.maycon.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
