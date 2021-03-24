dbb780a6ad144cf7d0e3fd29bbecd7e71b6dbd87
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-24 08:39:29 -0300

    Produto e associacao many to many com categoria

---
+++ src/main/java/com/maycon/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.maycon.cursomc.repositories;
+
+import com.maycon.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
