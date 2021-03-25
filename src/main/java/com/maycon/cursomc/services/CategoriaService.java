0815138f625f6a98756081c8f61642e0310eefd1
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-25 09:35:18 -0300

    deletando uma categoria com DELETE

--- src/main/java/com/maycon/cursomc/services/CategoriaService.java
+++ src/main/java/com/maycon/cursomc/services/CategoriaService.java
@@ -3,7 +3,9 @@ package com.maycon.cursomc.services;
 import com.maycon.cursomc.domain.Categoria;
 import com.maycon.cursomc.repositories.CategoriaRepository;
 import com.maycon.cursomc.services.Exception.ObjectNotFoundException;
+import com.maycon.cursomc.services.Exception.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -29,5 +31,14 @@ public class CategoriaService {
     public Categoria update(Categoria obj) {
         find(obj.getId());
         return categoriaRepository.save(obj);
+    }
+
+    public void detele(Integer id) {
+        find(id);
+        try {
+            categoriaRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+        }
     }
 }
---
+++ src/main/java/com/maycon/cursomc/services/Exception/DataIntegrityException.java
@@ -0,0 +1,12 @@
+package com.maycon.cursomc.services.Exception;
+
+public class DataIntegrityException extends RuntimeException {
+
+    public DataIntegrityException(String msg) {
+        super(msg);
+    }
+
+    public DataIntegrityException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
