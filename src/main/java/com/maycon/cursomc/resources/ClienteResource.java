663b26b26d9b00c2e1f2f0ff02d70df3764693b0
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-26 15:28:31 -0300

    inserindo um novo Cliente com POST

--- src/main/java/com/maycon/cursomc/resources/ClienteResource.java
+++ src/main/java/com/maycon/cursomc/resources/ClienteResource.java
@@ -2,13 +2,16 @@ package com.maycon.cursomc.resources;
 
 import com.maycon.cursomc.domain.Cliente;
 import com.maycon.cursomc.domain.dto.ClienteDTO;
+import com.maycon.cursomc.domain.dto.ClienteNewDTO;
 import com.maycon.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,6 +26,16 @@ public class ClienteResource {
     public ResponseEntity<Cliente> find(@PathVariable Integer id) {
         Cliente obj = clienteService.find(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+        Cliente obj = clienteService.fromDTO(objDto);
+        obj = clienteService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
