b5245253647806b636ca1d369a8c3e2042c60c55
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-29 15:52:55 -0300

    MockEmailService com Logger

--- src/main/java/com/maycon/cursomc/config/TestConfig.java
+++ src/main/java/com/maycon/cursomc/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.maycon.cursomc.config;
 
 import com.maycon.cursomc.services.DBService;
+import com.maycon.cursomc.services.EmailService;
+import com.maycon.cursomc.services.MockEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,5 +19,10 @@ public class TestConfig {
     public boolean instantiateDatabase() throws Exception{
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new MockEmailService();
     }
 }
