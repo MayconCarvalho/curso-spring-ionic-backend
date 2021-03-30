688927e7bdbce39af1ed745ed773d5feb167da10
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-30 15:36:30 -0300

    salvando primeiro arquivo no s3 amazon

--- src/main/java/com/maycon/cursomc/CursomcApplication.java
+++ src/main/java/com/maycon/cursomc/CursomcApplication.java
@@ -1,11 +1,16 @@
 package com.maycon.cursomc;
 
+import com.maycon.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
