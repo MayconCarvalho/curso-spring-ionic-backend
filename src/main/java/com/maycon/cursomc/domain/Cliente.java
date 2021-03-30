f2ee5623ced2799b632f3e3dd295d79891e86654
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-30 16:39:55 -0300

    salvando url da imagem em Cliente

--- src/main/java/com/maycon/cursomc/domain/Cliente.java
+++ src/main/java/com/maycon/cursomc/domain/Cliente.java
@@ -41,6 +41,8 @@ public class Cliente implements Serializable {
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList<>();
 
+    private String imageUrl;
+
     public Cliente() {
         addPerfil(Perfil.CLIENTE);
     }
@@ -133,6 +135,14 @@ public class Cliente implements Serializable {
 
     public void addPerfil(Perfil perfil) {
         perfils.add(perfil.getCod());
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     @Override
